3aeb85150da5bab08526d532ed18966b91911238
subina01 <subinadhakal01@gmail.com>
2024-01-19 12:24:11 +0545

    first commit

---
+++ CRUDoperation/Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CRUDoperation.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDoperation.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
